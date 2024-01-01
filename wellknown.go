32f8587f098e558741962e2ebbc4dd74c6516cc8
navigaid <navigaid@gmail.com>
2024-01-01 11:42:40 +0000

    add WellKnownHealthMiddleware from webteleport/ufo/x

---
+++ wellknown.go
@@ -0,0 +1,17 @@
+package utils
+
+import "net/http"
+
+const WellKnownHealthPath = "/.well-known/health"
+
+func WellKnownHealthMiddleware(next http.Handler) http.Handler {
+	health := func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != WellKnownHealthPath {
+			next.ServeHTTP(w, r)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
+	return http.HandlerFunc(health)
+}
