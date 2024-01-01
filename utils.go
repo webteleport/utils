3aa2cc85275a7c42df201b09a9c8fd9834a0bb5d
navigaid <navigaid@gmail.com>
2024-01-01 13:11:24 +0000

    add ParseDomainCandidates

--- utils.go
+++ utils.go
@@ -94,3 +94,20 @@ func MaybeHyperlink(l string) string {
 	}
 	return l
 }
+
+// ParseDomainCandidates splits a path string like /a/b/cd/😏
+// into a list of subdomains: [a, b, cd, 😏]
+//
+// when result is empty, a random subdomain will be assigned by the server
+func ParseDomainCandidates(p string) []string {
+	var list []string
+	parts := strings.Split(p, "/")
+	for _, part := range parts {
+		dom := strings.Trim(part, " ")
+		if dom == "" {
+			continue
+		}
+		list = append(list, dom)
+	}
+	return list
+}
