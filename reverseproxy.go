b617161e9eb6fde5a1412cdb94e8c8f4ae18fa78
navigaid <navigaid@gmail.com>
2024-01-04 10:38:53 +0000

    reverseproxy: fix kubectl proxy: make https://-@upstream.com ignore user credential

--- reverseproxy.go
+++ reverseproxy.go
@@ -6,6 +6,14 @@ import (
 	"net/url"
 )
 
+// ReverseProxy
+//
+// - upstream: http://user:pass@example.com
+//   behavior: use upstream credential by default
+// - upstream: http://example.com
+//   behavior: passthrough client credential if any
+// - upstream: http://-@example.com
+//   behavior: don't pass any credential to upstream
 func ReverseProxy(addr string) http.Handler {
 	upstream, err := url.Parse(addr)
 	if err != nil {
@@ -14,6 +22,12 @@ func ReverseProxy(addr string) http.Handler {
 	rewrite := func(r *httputil.ProxyRequest) {
 		r.SetURL(upstream)
 		r.SetXForwarded()
+
+		// ignore credential when default upstream user set to -
+		if upstream.User.String() == "-" {
+			r.Out.Header.Del("Authorization")
+			return
+		}
 
 		// use client credentials if available
 		if r.In.Header.Get("Authorization") != "" {
