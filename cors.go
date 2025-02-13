7f9ef1bd2f25c5b4b6e9f0b6fc2db8ab31dff8aa
navigaid <navigaid@gmail.com>
2025-02-13 19:03:24 +0000

    cors: allow credentials

--- cors.go
+++ cors.go
@@ -7,5 +7,27 @@ import (
 )
 
 var AllowAllCorsMiddleware = func(next http.Handler) http.Handler {
-	return cors.AllowAll().Handler(next)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+		if origin == "" {
+			origin = "*"
+		}
+		config := cors.New(cors.Options{
+			AllowedOrigins: []string{
+				origin,
+			},
+			AllowedMethods: []string{
+				http.MethodHead,
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodPut,
+				http.MethodPatch,
+				http.MethodDelete,
+			},
+			AllowedHeaders:   []string{"*"},
+			AllowCredentials: true,
+		})
+
+		config.Handler(next).ServeHTTP(w, r)
+	})
 }
